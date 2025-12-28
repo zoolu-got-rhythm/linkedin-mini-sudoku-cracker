c93a1c2c5931dd097a177c77bab30ee1e50c7331
christo <christopher.pkm@gmail.com>
2025-12-28 23:10:35 +0000

    mvp for sudoku cracker (linkedin mini sudoku almost working)

---
+++ test.js
@@ -0,0 +1,46 @@
+import { spawn } from "child_process";
+
+export async function executeAspClingoSudokuLogicProgram() {
+  const inputPathLinkedinMiniSudokuSolver = "linkedinMiniSudokuSolver.lp";
+  const inputPathLinkedinMiniSudokuCluesFacts = "linkedinMiniSudokuCluesFacts.lp";
+
+  return new Promise((resolve, reject) => {
+    const clingo = spawn("clingo", [
+      inputPathLinkedinMiniSudokuSolver,
+      inputPathLinkedinMiniSudokuCluesFacts,
+    ]);
+
+    let stdout = "";
+    let stderr = "";
+
+    clingo.stdout.on("data", (data) => {
+      const s = data.toString();
+      stdout += s;
+    //   console.log("stdout" + s);
+    });
+
+    clingo.stderr.on("data", (data) => {
+      const s = data.toString();
+      stderr += s;
+    //   console.error("stderr" + s);
+    });
+
+    // clingo.on("error", (err) => reject(err)); // does this do anything? or is it needed?
+
+    clingo.on("close", (code) => {
+      console.log(`Clingo exited with ${code}`);
+      const result = { code, stdout, stderr };
+      if (code === 0 || code === 30) resolve(result);
+      else reject(result);
+    });
+  });
+}
+
+// (async () => {
+//   try {
+//     let result = await executeAspClingoSudokuLogicProgram();
+//     console.log("result is", result);
+//   } catch (error) {
+//     console.log("error is", error);
+//   }
+// })();
